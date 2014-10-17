55d36446e02d8e68446a2729c19d3923e3cadb1e
Daniel Barney <daniel@pagodabox.com>
2014-10-17 15:30:11 -0600

    fixed spelling error

--- package.lua
+++ package.lua
@@ -1,5 +1,5 @@
 return
-	{description = "A simple sentinal aware proxy for high availability redis clusters"
+	{description = "A simple sentinel aware proxy for high availability redis clusters"
 	,version = "0.0.0"
 	,main = "redis_proxy.lua"
 }
